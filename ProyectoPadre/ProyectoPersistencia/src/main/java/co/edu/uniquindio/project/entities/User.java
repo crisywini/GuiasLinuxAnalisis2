a5f0319bfdd9233643bb3db40975e3f621876119
Crisi <harmaharcri@hotmail.com>
2020-03-31 18:49:15 -0500

    USER PARAMETRIZATION
    I parameterized the JPA Entity User, by adding the email as unique and not nullable, the password only not nullable and the code as not nullable and with a length by 10 digits (character)

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/User.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/User.java
@@ -9,18 +9,38 @@ import javax.persistence.*;
  *
  */
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // Me preocupa esa advertencia
 @MappedSuperclass
 public class User implements Serializable {
 
 	@Id
+	@Column(name = "code", nullable = false, length = 10)
 	private String code;
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
+	@Column(name = "password", nullable = false)
 	private String password;
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public User() {
 		super();
+	}
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param code     from User primary key
+	 * @param email    from User unique
+	 * @param password from User
+	 */
+	public User(String code, String email, String password) {
+		super();
+		this.code = code;
+		this.email = email;
+		this.password = password;
 	}
 
 	public String getCode() {
