81f7b43e3c1e71321777a1cd06ad76269a3207be
Crisi <harmaharcri@hotmail.com>
2020-03-31 19:38:38 -0500

    CITY PARAMETRIZATION
    I parameterized the JPA Entity City by adding the code name and not nullable, the name name and not nullable and the projects association name

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/City.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/City.java
@@ -15,14 +15,33 @@ import javax.persistence.*;
 public class City implements Serializable {
 
 	@Id
+	@Column(name = "code", nullable = false)
 	private int code;
+	@Column(name = "name", nullable = false)
 	private String name;
 	@OneToMany(mappedBy = "city")
+	@JoinColumn(name = "associated_projects")
 	private List<Project> projects;
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public City() {
 		super();
+	}
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param code     from {@link City} primary key and not nullable
+	 * @param name     from {@link City} not nullable
+	 * @param projects relationship with {@link Project}
+	 */
+	public City(int code, String name, List<Project> projects) {
+		this.code = code;
+		this.name = name;
+		this.projects = projects;
 	}
 
 	public int getCode() {
