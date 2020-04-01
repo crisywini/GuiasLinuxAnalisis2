b56f2984896379ae1b2f809c7eca9e9a98a117dc
Crisi <harmaharcri@hotmail.com>
2020-03-31 19:15:55 -0500

    CLIENT PARAMETRIZATION
    I parameterized the  JPA Entity Client, by adding the completeName not nullable, the phone number notnullable, and the configuration needed to the date of birth, and put it names of the relationships with project, could be wrong the another constructor method

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Client.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Client.java
@@ -16,21 +16,58 @@ import javax.persistence.*;
 
 public class Client extends User implements Serializable {
 
+	@Column(name = "complete_name", nullable = false)
 	private String completeName;
+	@Column(name = "phone_number", nullable = false)
 	private String phoneNumber;
+	@Temporal(TemporalType.DATE) // This ignore the hour, only the date is important
+	@Column(name = "date_of_birth", nullable = false)
 	private Date dateOfBirth;
 	@OneToMany(mappedBy = "clientCode")
+	@JoinColumn(name = "contacts")
 	private List<Contact> contacts;
 	@OneToMany(mappedBy = "clientCode")
+	@JoinColumn(name = "comments")
 	private List<Comment> comments;
 	@OneToMany(mappedBy = "clientRating")
+	@JoinColumn(name = "ratings")
 	private List<Rating> ratings;
 	@ManyToMany
+	@JoinColumn(name = "favorite_projects")
 	private List<Project> favoriteProjects;// Esta es la entidad propietaria
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public Client() {
 		super();
+	}
+
+	/**
+	 * Constructor method from Client
+	 * 
+	 * @param code             from User primary key not nullable
+	 * @param email            from User unique not nullable
+	 * @param password         from User not nullable
+	 * @param completeName     from Client not nullable
+	 * @param phoneNumber      from Client not nullable
+	 * @param dateOfBirth      from Client not nullable
+	 * @param contacts         from relationship with Project
+	 * @param comments         from relationship with project
+	 * @param ratings          from relationship with project
+	 * @param favoriteProjects relationship with project
+	 */
+	public Client(String code, String email, String password, String completeName, String phoneNumber, Date dateOfBirth,
+			List<Contact> contacts, List<Comment> comments, List<Rating> ratings, List<Project> favoriteProjects) {
+		super(code, email, password);
+		this.completeName = completeName;
+		this.phoneNumber = phoneNumber;
+		this.dateOfBirth = dateOfBirth;
+		this.contacts = contacts;
+		this.comments = comments;
+		this.ratings = ratings;
+		this.favoriteProjects = favoriteProjects;
 	}
 
 	public String getCompleteName() {
