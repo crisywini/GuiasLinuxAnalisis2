c0cfc8546651b6063c22c9b1f28667cf371a0995
Crisi <harmaharcri@hotmail.com>
2020-04-01 13:21:10 -0500

    CONSTRUCTOR METHOD IN ADMINISTRATOR
    Se añadió el constructor con parámetros a la entidad JPA Administrator.

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Administrator.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Administrator.java
@@ -12,11 +12,24 @@ import javax.persistence.*;
 
 public class Administrator extends User implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public Administrator() {
 		super();
 	}
-   
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param code     from {@link User} primary key and not nullable
+	 * @param email    from {@link User} unique and not nullable
+	 * @param password from {@link User} not nullable
+	 */
+	public Administrator(String code, String email, String password) {
+		super(code, email, password);
+	}
+
 }
