f6c23dcccb12cf1b72918e4ad9f91247dea8c3fc
Luisa Cotte <luisacotte18@hotmail.com>
2020-04-01 18:16:27 -0500

    COMMENT
    Se creó y documentó el constructor de la clase Comment

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Comment.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Comment.java
@@ -17,25 +17,46 @@ public class Comment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "code")
 	private int code;
-	
+
 	@Lob
 	@Column(name = "comment", nullable = false)
 	private String comment;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "date", nullable = false)
 	private Date date;
-	
+
 	@ManyToOne
+	@JoinColumn(name = "client_code")
 	private Client clientCode;
-	
+
 	@ManyToOne
+	@JoinColumn(name = "project_code")
 	private Project projectCode;
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public Comment() {
 		super();
+	}
+
+	/**
+	 * Constructor method from comment
+	 * 
+	 * @param code        from {@link Comment} primary key, and generated value
+	 * @param comment     from {@link Comment} not nullable
+	 * @param date        from {@link Comment} not nullable
+	 * @param clientCode  from relationship with {@link Client}
+	 * @param projectCode from relationship with {@link Project}
+	 */
+
+	public Comment(int code, String comment, Date date, Client clientCode, Project projectCode) {
+		super();
+		this.code = code;
+		this.comment = comment;
+		this.date = date;
+		this.clientCode = clientCode;
+		this.projectCode = projectCode;
 	}
 
 	public int getCode() {
