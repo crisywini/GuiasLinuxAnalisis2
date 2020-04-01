36dbca13319b1cc8535145a2072a27e179826a0b
Crisi <harmaharcri@hotmail.com>
2020-04-01 11:36:03 -0500

    PARAMETRIZACIÓN DE SERVICE
    Se parametrizó la entidad JPA Service, agregando etiquetas al codigo de nombre, y no puede ser nulo, también al nombre que tiene que ser unico y también a los proyectos asociados, se agregó el constructor con parámetros necesarios. aún no se sabe de que el nombre sea único.

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Service.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Service.java
@@ -14,24 +14,44 @@ import javax.persistence.*;
 
 public class Service implements Serializable {
 
-	   
 	@Id
+	@Column(name = "code", nullable = false)
 	private int code;
+	@Column(name = "name", nullable = false, unique = true) // ask for the unique
 	private String name;
 	@ManyToMany
+	@JoinColumn(name = "associated_projects")
 	private List<Project> projects;
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public Service() {
 		super();
-	}   
+	}
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param code     from {@link Service} primary key and not nullable
+	 * @param name     from {@link Service} not nullable and unique
+	 * @param projects relationship with {@link Project}
+	 */
+	public Service(int code, String name, List<Project> projects) {
+		this.code = code;
+		this.name = name;
+		this.projects = projects;
+	}
+
 	public int getCode() {
 		return this.code;
 	}
 
 	public void setCode(int code) {
 		this.code = code;
-	}   
+	}
+
 	public String getName() {
 		return this.name;
 	}
@@ -39,11 +59,13 @@ public class Service implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public List<Project> getProjects() {
 		return projects;
 	}
+
 	public void setProjects(List<Project> projects) {
 		this.projects = projects;
 	}
-   
+
 }
