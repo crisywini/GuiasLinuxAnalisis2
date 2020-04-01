69f1588603ce14541fe57b15e783b94093f32b2a
Crisi <harmaharcri@hotmail.com>
2020-04-01 12:46:09 -0500

    PARAMETRIZACIÓN DE PROJECT
    Se parametrizó la entidad JPA Project añadiendo los nombres a cada atributo y las propiedades de not nullable a: code, name, latitude, length y description, adicional a eso se eligió el tamaño más grande en el atributo description

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Project.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Project.java
@@ -15,31 +15,83 @@ import javax.persistence.*;
 public class Project implements Serializable {
 
 	@Id
+	@Column(name = "code", nullable = false)
 	private int code;
+	@Column(name = "name", nullable = false)
 	private String name;
+	@Column(name = "latitude", nullable = false)
 	private double latitude;
+	@Column(name = "length", nullable = false)
 	private double length;
+	@Lob
+	@Column(name = "description", nullable = false)
 	private String description;
 	@ManyToMany(mappedBy = "projects") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_services")
 	private List<Service> services;
 	@ManyToOne
+	@JoinColumn(name = "associated_estate_agency")
 	private EstateAgency estateAgency;// Esta es la entidad propietaria
 	@ManyToOne
+	@JoinColumn(name = "associated_city")
 	private City city;
 	@OneToMany(mappedBy = "project") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_dwellings")
 	private List<Dwelling> dwellings;
 	@OneToMany(mappedBy = "projectCode") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_contacts")
 	private List<Contact> contacts;
 	@OneToMany(mappedBy = "projectCode") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_comments")
 	private List<Comment> comments;
 	@OneToMany(mappedBy = "projectRating") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_ratings")
 	private List<Rating> ratings;
 	@ManyToMany(mappedBy = "favoriteProjects") // Esta es la entidad no propietaria
+	@JoinColumn(name = "associated_favorite_clients")
 	private List<Client> favoriteClients;
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor method
+	 */
 	public Project() {
 		super();
+	}
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param code            from {@link Project} primary key and not nullable
+	 * @param name            from {@link Project} not nullable
+	 * @param latitude        from {@link Project} not nullable
+	 * @param length          from {@link Project} not nullable
+	 * @param description     from {@link Project} big description and not nullable
+	 * @param services        from relationship with {@link Service}
+	 * @param estateAgency    relationship with {@link EstateAgency}
+	 * @param city            relationship with {@link City}
+	 * @param dwellings       from relationship with {@link Dwelling}
+	 * @param contacts        from relationship with {@link Contact}
+	 * @param comments        from relationship with {@link Comment}
+	 * @param ratings         from relationship with {@link Rating}
+	 * @param favoriteClients from relationship with {@link Client}
+	 */
+	public Project(int code, String name, double latitude, double length, String description, List<Service> services,
+			EstateAgency estateAgency, City city, List<Dwelling> dwellings, List<Contact> contacts,
+			List<Comment> comments, List<Rating> ratings, List<Client> favoriteClients) {
+		this.code = code;
+		this.name = name;
+		this.latitude = latitude;
+		this.length = length;
+		this.description = description;
+		this.services = services;
+		this.estateAgency = estateAgency;
+		this.city = city;
+		this.dwellings = dwellings;
+		this.contacts = contacts;
+		this.comments = comments;
+		this.ratings = ratings;
+		this.favoriteClients = favoriteClients;
 	}
 
 	public int getCode() {
