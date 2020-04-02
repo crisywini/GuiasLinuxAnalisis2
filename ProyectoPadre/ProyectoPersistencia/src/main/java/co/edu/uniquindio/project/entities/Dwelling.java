75f32123004b898ae3e0fa6d67bea3ffedff8122
Luisa Cotte <luisacotte18@hotmail.com>
2020-04-01 19:08:58 -0500

    DWEELING
    code with name, not nullable and length mx = 10; urlIMage with name and not nullable; area with name, not nillable and length mx = 10; price with name and not nullable; description with name, not nullable and Lob; numRomms with name, not nullable and length mx = 10; numBathrooms with name, not nullable and length mx = 10; type with name and not nullable, this is enum; project with name.
    Constructor method from Dweling, document method.

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Dwelling.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Dwelling.java
@@ -15,20 +15,68 @@ import javax.persistence.*;
 public class Dwelling implements Serializable {
 
 	@Id
+	@Column(name = "code", nullable = false, length = 10)
 	private int code;
+
+	@Column(name = "urlImage", nullable = false)
 	private String urlImage;
+
+	@Column(name = "area", nullable = false, length = 10)
 	private double area;
+
+	@Column(name = "price", nullable = false)
 	private double price;
+
+	@Lob
+	@Column(name = "description", nullable = false)
 	private String description;
+
+	@Column(name = "numRooms", nullable = false, length = 10)
 	private int numRooms;
+
+	@Column(name = "numBathrooms", nullable = false, length = 10)
 	private int numBathrooms;
+
+	@Column(name = "type", nullable = false)
 	private Type type;
+
 	@ManyToOne // Entidad propietaria
+	@JoinColumn(name = "project")
 	private Project project;
+
 	private static final long serialVersionUID = 1L;
 
 	public Dwelling() {
 		super();
+	}
+
+	/**
+	 * Constructor method from Dwelling
+	 * 
+	 * @param code         from {@link Dwelling} primary key, not nullable and
+	 *                     length max = 10
+	 * @param urlImage     from {@link Dwelling} not nullable
+	 * @param area         from {@link Dwelling} not nullable and length max = 10
+	 * @param price        from {@link Dwelling} not nullable
+	 * @param description  from {@link Dwelling} not nullable and Lob
+	 * @param numRooms     from {@link Dwelling} not nullable and length max = 10
+	 * @param numBathrooms from {@link Dwelling} not nullable and length max = 10
+	 * @param type         from {@link Dwelling} not nullable
+	 * @param project      from relationship with {@link Project}
+	 */
+
+	public Dwelling(int code, String urlImage, double area, double price, String description, int numRooms,
+			int numBathrooms, Type type, Project project) {
+		super();
+		this.code = code;
+		this.urlImage = urlImage;
+		this.area = area;
+		this.price = price;
+		this.description = description;
+		this.numRooms = numRooms;
+		this.numBathrooms = numBathrooms;
+		this.type = type;
+		this.project = project;
 	}
 
 	public int getCode() {
