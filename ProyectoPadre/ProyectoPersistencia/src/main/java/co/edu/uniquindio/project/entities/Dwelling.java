844435f3a2f1c4e5ea8c5d0f1cce5dfd584d7063
Crisi <harmaharcri@hotmail.com>
2020-04-04 11:36:09 -0500

    UPDATE DWELLING
    I update the JPA Entity Dwelling by adding the hashcode, equals and tostring override methods

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Dwelling.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/Dwelling.java
@@ -151,4 +151,33 @@ public class Dwelling implements Serializable {
 		this.project = project;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + code;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dwelling other = (Dwelling) obj;
+		if (code != other.code)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Dwelling [code=" + code + ", urlImage=" + urlImage + ", area=" + area + ", price=" + price
+				+ ", description=" + description + ", numRooms=" + numRooms + ", numBathrooms=" + numBathrooms
+				+ ", type=" + type + ", project=" + project + "]";
+	}
+
 }
