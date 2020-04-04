9fb477235e3010ff5dd81898b9978ddbeed2de06
Crisi <harmaharcri@hotmail.com>
2020-04-04 11:28:09 -0500

    UPDATE USER
    I update the User JPA Entity by adding the hashcode equals and toString override methods

--- ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/User.java
+++ ProyectoPadre/ProyectoPersistencia/src/main/java/co/edu/uniquindio/project/entities/User.java
@@ -67,4 +67,35 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [code=" + code + ", email=" + email + ", password=" + password + "]";
+	}
+	
+
 }
