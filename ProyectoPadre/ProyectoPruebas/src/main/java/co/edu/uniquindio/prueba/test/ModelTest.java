15bf7d2423c04e5086bcbdda57c897064cc2ad55
Crisi <harmaharcri@hotmail.com>
2020-04-05 21:49:56 -0500

    UPDATE PROJECT
    I update the project for the first final task

--- ProyectoPadre/ProyectoPruebas/src/main/java/co/edu/uniquindio/prueba/test/ModelTest.java
+++ ProyectoPadre/ProyectoPruebas/src/main/java/co/edu/uniquindio/prueba/test/ModelTest.java
@@ -4,8 +4,6 @@ import java.text.ParseException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,12 +17,9 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import co.edu.uniquindio.entidades.Genero;
-import co.edu.uniquindio.entidades.PersonaEntidades;
 import co.edu.uniquindio.project.entities.Administrator;
 import co.edu.uniquindio.project.entities.City;
 import co.edu.uniquindio.project.entities.Client;
@@ -66,62 +61,6 @@ public class ModelTest {
 						"META-INF/persistence.xml")
 
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-	}
-
-	/**
-	 * Generar test.
-	 */
-	@Test
-	public void generarTest() {
-
-	}
-
-	/**
-	 * Probar persistencia.
-	 *
-	 * @throws ParseException the parse exception
-	 */
-	@Ignore
-	@Test
-	@Transactional(value = TransactionMode.ROLLBACK)
-	/**
-	 * La manera de ejecutar la prueba: en Modo COMMIT comprueba que la inserción en
-	 * la base de datos sea correcta y deja la persona guardada, en modo ROLLBACK lo
-	 * que hace es modificarla y despues lo elimina, lo que ideal para las pruebas,
-	 * para que no se almacenen en la base de datos
-	 */
-	public void probarPersistencia() throws ParseException {
-
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
-		Date fecha = sdf.parse("15/11/1999");
-
-		Map<String, String> telefonos = new HashMap<String, String>();
-		telefonos.put("Casa", "2641965");
-		telefonos.put("Celular", "3147579868");
-
-		PersonaEntidades persona = new PersonaEntidades();
-		persona.setCedula("123456");
-		persona.setNombre("Cristian");
-		persona.setApellido("Sánchez");
-		persona.setEmail("harmaharsa@hotmail.com");
-		persona.setGenero(Genero.ANDROGINO);
-		persona.setFechaNacimiento(fecha);
-		persona.setNumerosDeTelefono(telefonos);
-
-		entityManager.persist(persona);
-		entityManager.flush();// Obliga a la transacción para que se mande al motor de la base de datos
-
-		PersonaEntidades newPersona2 = new PersonaEntidades();
-		newPersona2.setCedula("12346");
-		newPersona2.setNombre("Juan");
-		newPersona2.setApellido("Loaiza");
-		newPersona2.setEmail("jp4532@hotmail.com");
-		newPersona2.setFechaNacimiento(fecha);
-		newPersona2.setNumerosDeTelefono(telefonos);
-
-		entityManager.persist(newPersona2);
-		entityManager.flush();
-
 	}
 
 	// --------------------------------Test persist----------------------------- //
