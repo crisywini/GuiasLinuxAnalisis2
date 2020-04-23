3a5b0cc94024c16f93c571b3b9765fe0288a7884
Crisi <harmaharcri@hotmail.com>
2020-04-23 11:03:05 -0500

    UPDATE ADMINISTRATOR EJB REMOTE
    I have to add more administrator EJB requirements

--- ProyectoPadre/ProyectoDeNegocio/src/main/java/co/edu/uniquindio/project/AdministratorEJBRemote.java
+++ ProyectoPadre/ProyectoDeNegocio/src/main/java/co/edu/uniquindio/project/AdministratorEJBRemote.java
@@ -1,14 +1,22 @@
 package co.edu.uniquindio.project;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
+
 import co.edu.uniquindio.project.exceptions.AuthenticationException;
+import co.edu.uniquindio.project.exceptions.NonexistentUserException;
 import co.edu.uniquindio.project.exceptions.RepeatedUserException;
+import co.edu.uniquindio.unihogar.entities.EstateAgency;
 import co.edu.uniquindio.unihogar.entities.User;
 
 @Remote
 public interface AdministratorEJBRemote {
 	void logginUser(String code, String email, String password)throws RepeatedUserException;
 	User authenticateUser(String email, String password) throws AuthenticationException;
-
+	void createEstateAgency(String name, String code, String email, String password, String address)throws RepeatedUserException;
+	EstateAgency getEstateAgency(String code)throws NonexistentUserException;
+	void removeEstateAgency(String code) throws NonexistentUserException;
+	List<EstateAgency> listAgencies();
 }
