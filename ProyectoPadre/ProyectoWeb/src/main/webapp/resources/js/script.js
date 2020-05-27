1ca39a015de1143148dd89e35c91b3598fe08aeb
Crisi Sanchez Pineda <crisisanchezp@pop-os.localdomain>
2020-05-26 22:49:05 -0500

    CREATE THE TABLE FOR THE PROJECTS QUERY   I create the page for the query of the projects by name

--- ProyectoPadre/ProyectoWeb/src/main/webapp/resources/js/script.js
+++ ProyectoPadre/ProyectoWeb/src/main/webapp/resources/js/script.js
@@ -1,4 +1,41 @@
-function onLoadBody() {
+/**
+ * This method allows to initialize the needed components for the index page
+ * @returns void
+ */
+function onLoadIndex(){
+	
+	if(map_map !=null){
+		map_map.on("locationfound", onLocationFound);
+		//map_map.on("locationerror", onLocationError);
+		var icon = new L.Icon(
+				{
+					iconSize : [ 25, 41 ],
+					iconAnchor : [ 12, 41 ],
+					popupAnchor : [ 0, -45 ],
+					iconUrl : '/ProyectoWeb/javax.faces.resource/marker-icon.png.xhtml?ln=images',
+					shadowUrl : '/ProyectoWeb/javax.faces.resource/marker-shadow.png.xhtml?ln=images'
+				});
+
+		map_map.locate({
+			setView : true,
+			maxZoom : 16
+		});
+	}
+	
+	/**
+	 * listo, por un lado, lo del color de fondo podía ser cache, cuando haga un cambio de estilos
+	 * en el navegador recargue la página con: ctrl + F5, para que elimine cache.
+	 * Por otro lado para que funcionen las clases que dicen p-col- se necesita un archivo css adicional
+	 * el que se llama primeflex.css. Pero listo. 
+	 * Muchas gracias profe. Listo, cualquier cosa me escribe
+	 */
+	
+}
+/**
+ * This method allows to initialize the needed components for the registerProject page
+ * @returns void
+ */
+function onLoadProject() {
 	map_map.on("locationfound", onLocationFound);
 	map_map.on("locationerror", onLocationError);
 	map_map.on('click', onClickMarker);
